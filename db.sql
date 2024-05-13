b06b2aa19149e3c876f09c24faa521487b81e1a3
Ismail5478 <ismail.233040070@mail.unpas.ac.id>
2024-05-13 09:48:46 +0700

    db

--- db.sql
+++ db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: May 09, 2024 at 06:53 AM
+-- Generation Time: May 13, 2024 at 02:45 AM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
@@ -28,11 +28,10 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `hardware` (
-  `id` int NOT NULL,
+  `id_hardware` int NOT NULL,
   `nama` varchar(255) NOT NULL,
   `kategori` enum('ram','power suply','vga','hdd','ssd','motherboard','keyboard','mouse','monitor') NOT NULL,
   `deskripsi` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `avg_price` int NOT NULL,
   `gambar` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
@@ -40,9 +39,10 @@ CREATE TABLE `hardware` (
 -- Dumping data for table `hardware`
 --
 
-INSERT INTO `hardware` (`id`, `nama`, `kategori`, `deskripsi`, `avg_price`, `gambar`) VALUES
-(25, 'fasdds', 'ram', 'dsddsddd', 1324466, '8194Elmanuk.png'),
-(28, 'test', 'power suply', 'dasds', 1228398, '4218Char.png');
+INSERT INTO `hardware` (`id_hardware`, `nama`, `kategori`, `deskripsi`, `gambar`) VALUES
+(29, 'sad', 'vga', 'asdads', '6590Elmanuk.png'),
+(32, 'test', 'keyboard', 'sdasdasd', '1953Tek.png'),
+(33, 'aaaa', 'power suply', 'sadsda', '4283Char.png');
 
 -- --------------------------------------------------------
 
@@ -51,11 +51,19 @@ INSERT INTO `hardware` (`id`, `nama`, `kategori`, `deskripsi`, `avg_price`, `gam
 --
 
 CREATE TABLE `harga` (
-  `id` int NOT NULL,
+  `id_harga` int NOT NULL,
   `id_hardware` int NOT NULL,
-  `tanggal` date NOT NULL,
-  `harga` int NOT NULL
+  `avg_price` int NOT NULL,
+  `tanggal` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `harga`
+--
+
+INSERT INTO `harga` (`id_harga`, `id_hardware`, `avg_price`, `tanggal`) VALUES
+(7, 32, 65454564, '2024-05-14'),
+(8, 33, 6345434, '2024-05-12');
 
 -- --------------------------------------------------------
 
@@ -68,7 +76,7 @@ CREATE TABLE `user` (
   `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `role` enum('user','admin','super admin') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `role` enum('admin','super admin') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `gambar` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
@@ -77,9 +85,8 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `username`, `email`, `password`, `role`, `gambar`) VALUES
-(10, 'Ismail459', 'horispararoh457@gmail.com', '42a3b65b67c1f0bea6c484e675e0d80e', 'super admin', '3423Elmanuk.png'),
+(10, 'Ismail457', 'horispararoh457@gmail.com', '42a3b65b67c1f0bea6c484e675e0d80e', 'super admin', '3423Elmanuk.png'),
 (11, 'dasa', 'test@gmail.com', '3a08fe7b8c4da6ed09f21c3ef97efce2', 'admin', ''),
-(12, 'sara', 'alia@gmail.com', 'e11170b8cbd2d74102651cb967fa28e5', 'user', '444Char.png'),
 (13, 'gol', 'a@gmail.com', '0b5de470bdace90bd6cfb2541eb79f99', 'super admin', '1690Char.png');
 
 --
@@ -90,14 +97,13 @@ INSERT INTO `user` (`id`, `username`, `email`, `password`, `role`, `gambar`) VAL
 -- Indexes for table `hardware`
 --
 ALTER TABLE `hardware`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_hardware`);
 
 --
 -- Indexes for table `harga`
 --
 ALTER TABLE `harga`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `id_hardware_2` (`id_hardware`),
+  ADD PRIMARY KEY (`id_harga`),
   ADD KEY `id_hardware` (`id_hardware`);
 
 --
@@ -114,19 +120,19 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `hardware`
 --
 ALTER TABLE `hardware`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+  MODIFY `id_hardware` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 
 --
 -- AUTO_INCREMENT for table `harga`
 --
 ALTER TABLE `harga`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_harga` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- Constraints for dumped tables
@@ -136,7 +142,7 @@ ALTER TABLE `user`
 -- Constraints for table `harga`
 --
 ALTER TABLE `harga`
-  ADD CONSTRAINT `harga_ibfk_1` FOREIGN KEY (`id_hardware`) REFERENCES `hardware` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `harga_ibfk_1` FOREIGN KEY (`id_hardware`) REFERENCES `hardware` (`id_hardware`) ON DELETE CASCADE ON UPDATE RESTRICT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
